173592a255b4bfe53f5d9786a9b5d6c86af3f596
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-04-24 14:12:07 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51P5n7UGz3pLFtJboZAJilc41jkfDOT2xD0bVSBOXhsHyokL5N2lW8pE8eOXR10kCJ9q8ZDGx0fJ4YYYAJjNe7SnY00iJ7jUfAk',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
