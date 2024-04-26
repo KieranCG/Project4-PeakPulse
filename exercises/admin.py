a32ead56dd8387bec39d2d3d5f6e32ce3fd98c78
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-04-26 15:23:38 +0000

    customised exercises admin

--- exercises/admin.py
+++ exercises/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import Exercise, ExerciseCategory
 
 
-admin.site.register(Exercise)
-admin.site.register(ExerciseCategory)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'rating',
+        'category',
+    )
+
+    ordering = ('category',)
+
+
+class ExerciseCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(ExerciseCategory, ExerciseCategoryAdmin)
