4a59104ea757b2b7fe463359467a5d59f2b49194
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-04-26 15:33:00 +0000

    added exercises views and templates

--- exercises/views.py
+++ exercises/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Exercise
 
 # Create your views here.
+
+
+def all_exercises(request):
+    """ A view to show all exercises, including sorting and search queries """
+
+    exercises = Exercise.objects.all()
+
+    context = {
+        'exercises': exercises,
+    }
+
+    return render(request, 'exercises/exercises.html', context)
