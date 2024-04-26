71c1bc5dcbffb0df8e5350f437a4a70cb47c8ddd
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-04-26 10:53:23 +0000

    exercise- creating related database model pre load

--- exercise/admin.py
+++ exercise/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Exercise
 
-# Register your models here.
-admin.site.register(Exercise)
+
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'body_part',
+        'equipment',
+        'level',
+        'rating',
+    )
+
+    ordering = ('title',)
+
+
+admin.site.register(Exercise, ExerciseAdmin)
--- exercise/models.py
+++ exercise/models.py
@@ -3,22 +3,26 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    friendly_name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True, default="")
 
     def __str__(self):
         return self.name
 
+    def get_friendly_name(self):
+        return self.friendly_name
+
 
 class Exercise(models.Model):
-    exercise_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    type = models.CharField(max_length=100)
     body_part = models.CharField(max_length=100)
     equipment = models.CharField(max_length=100)
     level = models.CharField(max_length=100)
     rating = models.FloatField(blank=True, null=True)
     rating_desc = models.CharField(max_length=255, blank=True)
+
+    # ForeignKey relationship
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
