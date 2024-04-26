3c9ab7adcb945c9c74b5a2a7ff95e7e59636c7f2
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-04-26 09:57:35 +0000

    data model added, no relationship eastablished yet

--- exercise/models.py
+++ exercise/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Exercise(models.Model):
     exercise_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
