af11deb66863a8fd9ce42a2c75d48f1aeed9fb95
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-04-26 16:28:48 +0000

    pagination added to exercises template

--- exercises/views.py
+++ exercises/views.py
@@ -1,16 +1,32 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Exercise
-
-# Create your views here.
 
 
 def all_exercises(request):
     """ A view to show all exercises, including sorting and search queries """
 
-    exercises = Exercise.objects.all()
+    # Retrieve all exercises from the database
+    exercises_list = Exercise.objects.all()
+
+    # Number of exercises to display per page
+    items_per_page = 20  # Change this to 20 for 20 items per page
+
+    # Initialize Paginator object with queryset and items per page
+    paginator = Paginator(exercises_list, items_per_page)
+
+    # Get the current page number from the request, default to page 1
+    page_number = request.GET.get('page', 1)
+
+    try:
+        # Get the page object for the requested page number
+        page_obj = paginator.page(page_number)
+    except EmptyPage:
+        # If the requested page is out of range, deliver last page of results
+        page_obj = paginator.page(paginator.num_pages)
 
     context = {
-        'exercises': exercises,
+        'page_obj': page_obj,
     }
 
     return render(request, 'exercises/exercises.html', context)
