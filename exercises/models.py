874f3cdb0c4fed1cbf0e8bb46338930fe0150672
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-04-27 14:31:51 +0000

    added extra fields to exercise model to allow user diary creation - also small edits to currency

--- exercises/models.py
+++ exercises/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class ExerciseCategory(models.Model):
@@ -14,6 +15,7 @@ class ExerciseCategory(models.Model):
 
 
 class Exercise(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # ForeignKey to link each exercise with a user
     title = models.CharField(max_length=200)
     description = models.TextField()
     bodypart = models.CharField(max_length=100)
@@ -22,6 +24,15 @@ class Exercise(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     ratingdesc = models.CharField(max_length=200)
     category = models.ForeignKey(ExerciseCategory, null=True, blank=True, on_delete=models.SET_NULL)
+
+    # Additional Fields
+    sets = models.PositiveIntegerField(default=1)
+    repetitions = models.PositiveIntegerField(default=1)
+    duration = models.DurationField(null=True, blank=True)  # Duration in seconds
+    weight = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)  # Weight in kilograms
+    distance = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)  # Distance in kilometers
+    intensity = models.CharField(max_length=100, null=True, blank=True)  # Intensity level or heart rate
+    notes = models.TextField(null=True, blank=True)  # Additional notes or comments
 
     def __str__(self):
         return self.title
