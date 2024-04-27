90897ecebc59c3a638c66f3d379a3c9fbb9b1a04
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-04-27 16:25:56 +0000

    rendered exercise logs correctly

--- exercises/urls.py
+++ exercises/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_exercises, name='exercises'),
     path('<int:exercise_id>', views.exercise_detail, name='exercise_detail'),
     path('logs/', views.all_exercise_logs, name='exercise_logs'),  # URL for displaying all exercise logs
+    path('create/', views.create_exercise_log, name='create_exercise_log'),
 ]
