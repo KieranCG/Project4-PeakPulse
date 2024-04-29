af68e555cac7734511e70366cea7da3429160ad8
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-04-29 09:25:26 +0000

    wired up urls at project level as well as rendered basic template for the community wall

--- community/views.py
+++ community/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def community_view(request):
+    """ A view to show all posts, including sorting """
+    posts = Post.objects.all()
+    return render(request, 'community/community.html', {'posts': posts})
--- peakpulse/urls.py
+++ peakpulse/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('exercises/', include('exercises.urls')),
+    path('community/', include('community.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
