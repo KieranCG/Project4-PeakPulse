cfc0f1dc37c692dd7b918e83d59776674e5c03b4
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-05-01 14:47:35 +0000

    url, view and outline of template created for subscription_checkout

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -6,4 +6,5 @@ app_name = 'subscriptions'
 
 urlpatterns = [
     path('', views.subscriptions_view, name='subscriptions'),
+    path('checkout/', views.subscription_checkout, name='subscription_checkout'),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def subscriptions_view(request):
     """ View function to render subscriptions.html template """
     return render(request, 'subscriptions/subscriptions.html')
+
+
+def subscription_checkout(request):
+    """ View logic for the subscription checkout page """
+    return render(request, 'subscriptions/subscriptions_checkout.html')
