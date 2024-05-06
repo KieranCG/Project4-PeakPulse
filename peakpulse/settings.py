4afc1d4bb4325829406d6b42e502cd1c886aca26
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-05-06 12:18:49 +0000

    deployment debugging in heroku

--- peakpulse/settings.py
+++ peakpulse/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-kierancg-project4peakpu-01gevits1sg.ws-eu111.gitpod.io',
