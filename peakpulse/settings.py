d0a1f8a95318d703b8f9fe9c57fd4c66df18b306
Kieran Gallagher <kierano777@hotmail.co.uk>
2024-05-06 12:09:53 +0000

    debugging bad request 400 in heroku

--- peakpulse/settings.py
+++ peakpulse/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-kierancg-project4peakpu-01gevits1sg.ws-eu111.gitpod.io',
